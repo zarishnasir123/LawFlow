969e0da53923c0c8e854f5b8318d2e71ee796e15
Zarish Nasir <zarishnasir12345@gmail.com>
2026-01-01 14:22:13 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline">LawFlow</h1>
+    <p className="bg-green-500">LawFlow is a web application that helps lawyers to manage their cases and clients.</p>
+     
+    </>
+  )
+}
+
+export default App
