25ee03115db394ce8318b5667f1bbd0092a9c23b
Zarish Nasir <zarishnasir12345@gmail.com>
2026-01-03 16:19:56 +0500

    updated

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,7 +7,6 @@ function App() {
     <p className="bg-green-500">LawFlow is a web application that helps lawyers to manage their cases and clients.</p>
     <h1 className="text-2xl font-semibold">Welcome to LawFlow</h1>
     <h2>
-      Your all-in-one solution for legal case management. changed by umar awan edhbj ehgdyegbl
       Your all-in-one solution for legal case management changed test by zarish
     </h2>
 
