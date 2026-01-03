a27292159fd226da8342a7b5441cdd1c6e7fe3ac
umar Awan <Umarfarooq6153@gmail.com>
2026-01-03 14:49:24 +0500

    chanfe

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <p className="bg-green-500">LawFlow is a web application that helps lawyers to manage their cases and clients.</p>
     <h1 className="text-2xl font-semibold">Welcome to LawFlow</h1>
     <h2>
-      Your all-in-one solution for legal case management. changed by umar
+      Your all-in-one solution for legal case management. changed by umar awan
     </h2>
      
     </>
